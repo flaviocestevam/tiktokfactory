0860d12ada0f3ddf02efb67b1f36495542a1b5f3
flaviocestevam <flaviocostaestevam@gmail.com>
2026-08-02 02:43:21 -0300

    Ativa analisador estruturado prioritário

--- src/lib/tiktok.server.ts
+++ src/lib/tiktok.server.ts
@@ -1,5 +1,5 @@
 // Fonte única da importação de produtos do TikTok Shop.
-export { analisarProdutoTikTokShop } from "./tiktok/product-analyzer.server";
+export { analisarProdutoTikTokShop } from "./tiktok/product-analyzer-v2.server";
 export { validarUrlTikTok, ehLinkPermitido } from "./tiktok/validate-url.server";
 export { normalizarLinkTikTokShop } from "./tiktok/normalize-url.server";
 export { CAMPOS_PRODUTO } from "./tiktok/types";
