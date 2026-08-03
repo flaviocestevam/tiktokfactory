f50b84440769403947532dac2ffb78956cc93c63
flaviocestevam <flaviocostaestevam@gmail.com>
2026-08-03 06:12:40 -0300

    refactor: remove automatic product extraction server function

--- src/lib/produtos.functions.ts
+++
@@ -1,8 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-import { z } from "zod";
-import { analisarProdutoTikTokShop } from "./tiktok.server";
-
-/** Motor único: qualquer link de produto do TikTok Shop, de qualquer país. */
-export const analisarProduto = createServerFn({ method: "POST" })
-  .validator((i: unknown) => z.object({ url: z.string().min(8).max(2000) }).parse(i))
-  .handler(async ({ data }) => analisarProdutoTikTokShop(data.url));
